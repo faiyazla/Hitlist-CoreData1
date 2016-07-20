20f13b5c204318a091998d8b00d199bb58ba2e37
Faiyaz Ahmed <faiyaz_ahmed@Faiyazs-MacBook-Pro.local>
2016-07-20 10:27:48 +0545

    Simple Core Data Application

---
+++ Person+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Person+CoreDataProperties.swift
+//  Hitlist-coreData1
+//
+//  Created by Faiyaz Ahmed on 7/20/16.
+//  Copyright © 2016 Faiyaz Ahmed. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var name: String?
+
+}
---
+++ Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  Hitlist-coreData1
+//
+//  Created by Faiyaz Ahmed on 7/20/16.
+//  Copyright © 2016 Faiyaz Ahmed. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Person: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
