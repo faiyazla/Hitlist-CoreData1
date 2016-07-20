ae68c005d59deb452f7b61362390ff17334bdb3c
Faiyaz <faiyaz_ahmed@faiyazs-macbook-pro.local>
2016-07-20 10:07:57 +0545

    Initial Commit

---
+++ Hitlist-coreData1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hitlist-coreData1
+//
+//  Created by Faiyaz Ahmed on 7/20/16.
+//  Copyright © 2016 Faiyaz Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
